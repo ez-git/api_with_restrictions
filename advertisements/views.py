c4cf8481b24292b64f73652bf1cef17edcab80bd
EZ <dw04@ya.ru>
2023-06-04 16:32:00 +0300

    Initial commit

---
+++ advertisements/views.py
@@ -0,0 +1,28 @@
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import OrderingFilter
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.throttling import UserRateThrottle, AnonRateThrottle
+from rest_framework.viewsets import ModelViewSet
+
+from .filters import AdvertisementFilter
+from .models import Advertisement
+from .permissions import IsOwnerOrReadOnly
+from .serializers import AdvertisementSerializer
+
+
+class AdvertisementViewSet(ModelViewSet):
+    """ViewSet для объявлений."""
+
+    queryset = Advertisement.objects.all()
+    serializer_class = AdvertisementSerializer
+    filter_backends = [DjangoFilterBackend, OrderingFilter]
+    filterset_class = AdvertisementFilter
+    ordering_fields = ["created_at", "title"]
+    throttle_classes = [UserRateThrottle, AnonRateThrottle]
+    permission_classes = [IsAuthenticated, IsOwnerOrReadOnly]
+
+    def get_permissions(self):
+        """Получение прав для действий."""
+        if self.action in ["create", "update", "partial_update", "destroy"]:
+            return [IsAuthenticated(), IsOwnerOrReadOnly()]
+        return []
